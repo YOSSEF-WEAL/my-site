3e2b0cd610dba32b0d01e0a7b61326c089c7f18e
YOSSEF-WEAL <162459437+YOSSEF-WEAL@users.noreply.github.com>
2024-11-29 13:18:15 +0200

    Update main.js

--- js-file/main.js
+++ js-file/main.js
@@ -84,3 +84,15 @@ theme_switch.addEventListener("click", () =>
 });
 
 
+// Spinner
+const spinner = document.querySelector('.spinner-wrapper')
+window.addEventListener('load', () =>
+{
+    spinner.style.opacity = '0';
+    setTimeout(() =>
+    {
+        spinner.style.display = 'none';
+    }, 100);
+})
+
+
