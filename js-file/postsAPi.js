86ac6246bc6bb04767c3a09ac38200a0333e30cf
Yossef weal <162459437+YOSSEF-WEAL@users.noreply.github.com>
2025-01-29 15:29:35 +0200

    Add files via upload

---
+++ js-file/postsAPi.js
@@ -0,0 +1,57 @@
+const countriesContainer = document.querySelector('.countriess');
+console.log(countriesContainer);
+
+async function fetchCategories()
+{
+    const response = await fetch("https://a3raff.com/Yossefprofile/wp-json/wp/v2/categories");
+    const categories = await response.json();
+    const categoryMap = {};
+    categories.forEach(cat =>
+    {
+        categoryMap[cat.id] = cat.name;
+
+
+        // console.log(categoryMap);
+    });
+    return categoryMap;
+}
+
+async function fetchPosts()
+{
+    try
+    {
+        const categoryMap = await fetchCategories();
+        const response = await fetch(`https://a3raff.com/Yossefprofile/wp-json/wp/v2/posts?_embed&per_page=12`);
+        const data = await response.json();
+
+        data.forEach(post =>
+        {
+            const imageUrl = post._embedded?.["wp:featuredmedia"]?.[0]?.source_url || "No Image";
+            const projectLink = post.acf?.link_progect || "#";
+
+            const postCategories = post.categories.map(catId => categoryMap[catId] || "غير معروف");
+            const html = `
+                <div class="projec_box " data-aos="fade-up" ft">
+                    <img  src="${imageUrl}" alt="">
+                    <div  class="text">
+                        <h4><span>${postCategories.join(", ")}</span></h4>
+                        <a target="_blank" href="${projectLink}">${post.title.rendered}</a>
+                        <p>${post.excerpt.rendered}</p>
+
+                        <a target="_blank" href="${projectLink}" class="link"><i
+                                class="fa-solid fa-arrow-up"></i></a>
+                    </div>
+                </div>
+            `;
+
+            countriesContainer.insertAdjacentHTML('afterbegin', html);
+        });
+
+    } catch (error)
+    {
+        console.error(`❌ Error fetching posts: ${error}`);
+    }
+}
+// ${post.link}
+
+fetchPosts();
