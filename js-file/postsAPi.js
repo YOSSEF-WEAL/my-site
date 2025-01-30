fa37de4a15967efd9931a3ba1ecab8e2de59e19a
Yossef weal <162459437+YOSSEF-WEAL@users.noreply.github.com>
2025-01-31 00:59:34 +0200

    Update postsAPi.js

--- js-file/postsAPi.js
+++ js-file/postsAPi.js
@@ -43,7 +43,7 @@ async function fetchPosts()
                         <a target="_blank" href="${projectLink}" class="link"><i
                                 class="fa-solid fa-arrow-up"></i></a>
                         <a style="width: fit-content; font-size: 20px; padding: 10px 20px; gap: 17px; rotate: 0deg;" href="./post.html?id=${post.id}" class="link">Read More 
-                        <i "rotate: 45deg; scale: 1.5;" class="fa-solid fa-arrow-up"></i></a>
+                        <i style= "rotate: 45deg !important; scale: 1.5; !important"  class="fa-solid fa-arrow-up"></i></a>
                     </div>
                     </div>
                 </div>
