70dc22952025827459d142a7b96b14a7576525b9
Yossef weal <162459437+YOSSEF-WEAL@users.noreply.github.com>
2025-01-30 15:39:25 +0200

    Add files via upload

---
+++ js-file/thePostIDfetch.js
@@ -0,0 +1,72 @@
+const params = new URLSearchParams(window.location.search);
+const postId = params.get("id");
+const countriesContainer = document.querySelector('.countriess');
+
+
+
+async function fetchCategories()
+{
+    const response = await fetch("https://a3raff.com/Yossefprofile/wp-json/wp/v2/categories");
+    const categories = await response.json();
+    const categoryMap = {};
+    categories.forEach(cat =>
+    {
+        categoryMap[cat.id] = cat.name;
+    });
+    return categoryMap;
+}
+
+async function fetchPosts()
+{
+    try
+    {
+        const categoryMap = await fetchCategories();
+        const response = await fetch(`https://a3raff.com/Yossefprofile/wp-json/wp/v2/posts/${postId}`);
+        const data = await response.json();
+        // ================== 
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data.content.rendered, 'text/html');
+        const imgElements = doc.querySelectorAll('img');
+        const imageUrls = Array.from(imgElements).map(img => img.src);
+        // ================== 
+        // === filter text data
+        const filteText = function (allContent)
+        {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(allContent, 'text/html');
+            return doc.body.textContent || "";
+        };
+
+        // ================== 
+        const projectLink = data.acf?.link_progect || "#";
+        const postCategories = data.categories.map(catId => categoryMap[catId] || "غير معروف");
+        const html = `
+            <div class="projec_box ${postCategories.join(" , ")}">
+                        <div class='imges'>
+            ${imageUrls.map((url) =>
+        {
+            return `<img src="${url}" alt="">`;
+        }).join('')}
+            </div>
+            <div class="text">
+                <h4><span>${postCategories.join(", ")}</span></h4>
+                <a target="_blank" href="${projectLink}">${data.title.rendered}</a>
+           
+          <p>      ${filteText(data.content.rendered)}</p>
+                <div class="links" style="gap: 10px; display: flex;">
+        <a target="_blank" href="${projectLink}" class="link"><i class="fa-solid fa-arrow-up"></i></a>
+    </div>
+            </div >
+
+            </div >
+        `;
+
+        countriesContainer.insertAdjacentHTML('afterbegin', html);
+    } catch (error)
+    {
+        console.error(`Error fetching post data ❌: ${error} `);
+    }
+}
+fetchPosts();
+
+
