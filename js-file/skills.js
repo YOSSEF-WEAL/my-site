79e56a7da1407b198ee427238ecc7714976b2762
YOSSEF-WEAL <yweal423@gmail.com>
2025-03-28 16:30:42 +0200

    add dinamec for skills

---
+++ js-file/skills.js
@@ -0,0 +1,36 @@
+const boxsSkills = document.querySelector('.technologys');
+
+
+async function fetchSkills()
+{
+    try
+    {
+        renderSpinner(boxsSkills);
+
+        const res = await fetch("https://a3raff.com/Yossefprofile/wp-json/wp/v2/skill");
+        const data = await res.json();
+
+        boxsSkills.innerHTML = '';
+
+
+        data.forEach(skill =>
+        {
+            const skillName = skill.acf?.skillName || "";
+            const skillImage = skill.acf?.skillImage || "";
+
+
+            const html = `<li class="technology"  data-aos="zoom-in">
+                          <img  class="imgTec" src="${skillImage}" alt="${skillName}">
+                        <p>${skillName}</p>
+                    </li>`
+            boxsSkills.insertAdjacentHTML('beforeend', html);
+        });
+
+    } catch (error)
+    {
+        console.error(error);
+    }
+
+}
+
+fetchSkills()
